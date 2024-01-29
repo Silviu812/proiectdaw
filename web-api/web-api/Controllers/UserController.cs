8539f8560298491b546c0d6f05b3ee5739a65433
Silviu812 <silviuandrei1056@gmail.com>
2024-01-29 14:48:09 +0200

    2

---
+++ web-api/web-api/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿namespace web_api.Controllers
+
+
+
+
+namespace web_api.Models
+{
+    public class UserController
+    {
+        [Key]
+        public int Id {  get; set; }
+        piblioc string UserName { get; set; } = String.Empty;
+    }
+}
---
+++ web-api/web-api/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_api.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public string Role { get; set; } = WebRoles.Member;
+    }
+    public static class WebRoles
+    {
+        public const string Member = "Membru";
+        public const string Admin = "Admin";
+    }
+}
